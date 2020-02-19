04aa8ab7b1046c195c53321f66795821d49cd08e
michelangelo17 <michelangelo.markus@gmail.com>
2020-02-19 13:42:11 -0800

    initial react app setup w/redux

---
+++ src/redux/store/index.ts
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from '../slices/authSlice'
+
+const store = configureStore({
+  reducer: authReducer,
+})
+
+export default store
