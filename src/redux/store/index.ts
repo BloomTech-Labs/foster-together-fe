418584ad9ed71aa11fe866e3a098c81e70fd6937
michelangelo17 <michelangelo.markus@gmail.com>
2020-02-19 15:46:31 -0800

    removed typescript

--- src/redux/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../slices/authSlice'
-
-const store = configureStore({
-  reducer: authReducer,
-})
-
-export default store
