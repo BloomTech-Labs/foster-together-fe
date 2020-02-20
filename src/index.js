7e9646931a733796b9c87e14d0c5cf3576054c88
B Severino <biancasev@gmail.com>
2020-02-20 17:48:45 -0500

    Added sentry back in

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import store from './store'
-// import * as Sentry from '@sentry/browser'
+import * as Sentry from '@sentry/browser'
 import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker'
 
-// Sentry.init({
-//   dsn: 'https://4a0af58b3e8b4350984d11fe9a1f8cb0@sentry.io/2715170',
-// })
+Sentry.init({
+  dsn: 'https://4a0af58b3e8b4350984d11fe9a1f8cb0@sentry.io/2715170',
+})
 
 ReactDOM.render(
   <Provider store={store}>
