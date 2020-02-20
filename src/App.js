5d832a99858e42e89deaefcd32a24124c9dcf666
michelangelo17 <michelangelo.markus@gmail.com>
2020-02-20 12:47:26 -0800

    testing CI

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>THIS IS A TEST!!!</p>
         <span>
           <span>Learn </span>
           <a
