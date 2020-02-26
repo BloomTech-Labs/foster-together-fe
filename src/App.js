440a8aa98888cbdbfa51b38321a306dc0e01b760
jarrodskahill <jarrodskahill@gmail.com>
2020-02-26 15:44:45 -0800

    converted back to normal

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import { CssBaseline } from '@material-ui/core'
 import { createBrowserHistory } from 'history'
 import ReactGA from 'react-ga'
 
-function initializeReactGA() {
-  ReactGA.initialize('UA-123791717-1')
-  ReactGA.pageview('/homepage')
-}
+const analytics = createBrowserHistory()
+// google analytics page view tracking
+analytics.listen(location => {
+  ReactGA.set({ page: location.pathname }) // Update the user's current page
+  ReactGA.pageview(location.pathname) // Record a pageview for the given page
+})
 
 function App() {
   return (
     <>
-      <Router>
+      <Router history={analytics}>
         <CssBaseline />
         <Route exact path='/signup' component={SignUp} />
       </Router>
