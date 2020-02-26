409283e0d3fe7b3ee56205c2c1315a8c1473a745
jarrodskahill <jarrodskahill@gmail.com>
2020-02-26 15:11:59 -0800

    bug fixes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import SignUp from './components/signUpForm/SignUpOverlay'
 import { Route, Router } from 'react-router-dom'
 import { CssBaseline } from '@material-ui/core'
 import { createBrowserHistory } from 'history'
+import ReactGA from 'react-ga'
 
 const analytics = createBrowserHistory()
 // google analytics page view tracking
-history.listen(location => {
+analytics.listen(location => {
   ReactGA.set({ page: location.pathname }) // Update the user's current page
   ReactGA.pageview(location.pathname) // Record a pageview for the given page
 })
@@ -14,7 +15,7 @@ history.listen(location => {
 function App() {
   return (
     <>
-      <Router history={history}>
+      <Router history={analytics}>
         <CssBaseline />
         <Route exact path='/signup' component={SignUp} />
       </Router>
