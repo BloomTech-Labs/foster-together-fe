c81918ebb8c2d44926613826d4e593fe18c4f2e1
michelangelo17 <michelangelo.markus@gmail.com>
2020-02-27 15:58:03 -0800

    google analytics test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import SignUp from './components/signUpForm/SignUpOverlay'
 import AdminDash from './components/AdminDash/AdminDashboard'
 import { Route } from 'react-router-dom'
 import { CssBaseline } from '@material-ui/core'
+import ReactGA from 'react-ga'
+
+function initializeAnalytics() {
+  ReactGA.initialize('UA-159166357-1')
+  ReactGA.pageview('/signup')
+}
 
 function App() {
+  initializeAnalytics()
   return (
     <>
       <CssBaseline />
--- src/index.js
+++ src/index.js
@@ -5,36 +5,35 @@ import store from './redux/store'
 import * as Sentry from '@sentry/browser'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { createBrowserHistory } from 'history'
-import ReactGA from 'react-ga'
+// import { createBrowserHistory } from 'history'
 
 Sentry.init({
   dsn: 'https://4a0af58b3e8b4350984d11fe9a1f8cb0@sentry.io/2715170',
 })
 
-const trackingId = 'UA-159166357-1'
-ReactGA.initialize(trackingId, {
-  debug: true,
-  titleCase: false,
-  gaOptions: {
-    userId: 123,
-    siteSpeedSampleRate: 100,
-  },
-})
-const analytics = createBrowserHistory()
-// google analytics page view tracking
-analytics.listen(location => {
-  ReactGA.set({ page: location.pathname }) // Update the user's current page
-  ReactGA.pageview(location.pathname) // Record a pageview for the given page
-  ReactGA.event({
-    category: 'User',
-    action: 'Sent message',
-  })
-})
+// const trackingId = 'UA-159166357-1'
+// ReactGA.initialize(trackingId, {
+//   debug: true,
+//   titleCase: false,
+//   gaOptions: {
+//     userId: 123,
+//     siteSpeedSampleRate: 100,
+//   },
+// })
+// const analytics = createBrowserHistory()
+// // google analytics page view tracking
+// analytics.listen(location => {
+//   ReactGA.set({ page: location.pathname }) // Update the user's current page
+//   ReactGA.pageview(location.pathname) // Record a pageview for the given page
+//   ReactGA.event({
+//     category: 'User',
+//     action: 'Sent message',
+//   })
+// })
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={analytics}>
+    <Router>
       <App />
     </Router>
   </Provider>,
