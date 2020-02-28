1212880c102519d8dfad5afb458702fb9f53a201
michelangelo17 <michelangelo.markus@gmail.com>
2020-02-27 16:08:49 -0800

    update to google analytics

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import AdminDash from './components/AdminDash/AdminDashboard'
 import { Route } from 'react-router-dom'
 import { CssBaseline } from '@material-ui/core'
 import ReactGA from 'react-ga'
+import { createBrowserHistory } from 'history'
 
 function initializeAnalytics() {
   ReactGA.initialize('UA-159166357-1')
-  ReactGA.pageview('/signup')
+  const analytics = createBrowserHistory()
+  // google analytics page view tracking
+  analytics.listen(location => {
+    ReactGA.set({ page: location.pathname }) // Update the user's current page
+    ReactGA.pageview(location.pathname) // Record a pageview for the given page
+    ReactGA.event({
+      category: 'User',
+      action: 'Sent message',
+    })
+  })
 }
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import store from './redux/store'
 import * as Sentry from '@sentry/browser'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
-// import { createBrowserHistory } from 'history'
 
 Sentry.init({
   dsn: 'https://4a0af58b3e8b4350984d11fe9a1f8cb0@sentry.io/2715170',
@@ -19,16 +18,6 @@ Sentry.init({
 //     userId: 123,
 //     siteSpeedSampleRate: 100,
 //   },
-// })
-// const analytics = createBrowserHistory()
-// // google analytics page view tracking
-// analytics.listen(location => {
-//   ReactGA.set({ page: location.pathname }) // Update the user's current page
-//   ReactGA.pageview(location.pathname) // Record a pageview for the given page
-//   ReactGA.event({
-//     category: 'User',
-//     action: 'Sent message',
-//   })
 // })
 
 ReactDOM.render(
