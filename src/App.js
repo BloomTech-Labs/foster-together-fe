e32d996e44b0017257c8c70aa4fd49784550a742
jarrodskahill <jarrodskahill@gmail.com>
2020-02-27 23:15:35 -0800

    had to convert back

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import { Route } from 'react-router-dom'
 import { CssBaseline } from '@material-ui/core'
 import { initGA, PageView } from './Analytics'
 
+function initializeAnalytics() {
+  ReactGA.initialize('UA-159166357-1')
+  const analytics = createBrowserHistory()
+  // google analytics page view tracking
+  analytics.listen(location => {
+    ReactGA.set({ page: location.pathname }) // Update the user's current page
+    ReactGA.pageview(location.pathname) // Record a pageview for the given page
+    ReactGA.event({
+      category: 'User',
+      action: 'Sent message',
+    })
+  })
+}
+
 function App() {
-  useEffect(() => {
-    initGA('UA-159198142-1')
-    PageView()
-  }, [])
+  initializeAnalytics()
   return (
     <>
       <CssBaseline />
