b64230c6a5f5faf029cd607242c4fb1189588cf8
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-03 09:54:42 -0800

    deleted some stuff that accidentally made it in.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { useEffect } from 'react'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Login from './components/logInForm/LoginForm'
-import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react'
+import { Security, ImplicitCallback } from '@okta/okta-react'
 import Home from './features/Dashboard/Home'
 import SignUp from './components/signUpForm/SignUpOverlay'
 import AdminDash from './components/AdminDash/AdminDashboard'
 import { CssBaseline } from '@material-ui/core'
-import ReactGA from 'react-ga'
-import { createBrowserHistory } from 'history'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import { initGA, PageView } from './Analytics'
 
@@ -36,20 +34,6 @@ const theme = createMuiTheme({
     },
   },
 })
-
-function initializeAnalytics() {
-  ReactGA.initialize('UA-159166357-1')
-  const analytics = createBrowserHistory()
-  // google analytics page view tracking
-  analytics.listen(location => {
-    ReactGA.set({ page: location.pathname }) // Update the user's current page
-    ReactGA.pageview(location.pathname) // Record a pageview for the given page
-    ReactGA.event({
-      category: 'User',
-      action: 'Sent message',
-    })
-  })
-}
 
 function onAuthRequired({ history }) {
   history.push('/login')
