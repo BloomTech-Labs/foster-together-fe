ed1c86afe44a657974be8b8d26a0a6c2418df771
B Severino <biancasev@gmail.com>
2020-03-05 18:32:21 -0500

    Fixed spacing on task cards, styled welcome message

--- src/components/AdminDash/adminDashStyles.js
+++ src/components/AdminDash/adminDashStyles.js
@@ -1,30 +1,31 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 
 export const DashContainer = styled.div`
-    align-items: center;
-    background-color: white;
-    min-height: 100vh;
-  `
+  align-items: center;
+  background-color: white;
+  min-height: 100vh;
+  margin: 0 32px;
+`
 
-    export const TableContain = styled.div`
-    position: relative;
-    left: 0vw;
-    top: 10vh;
-    height: auto;
-    width: '80vw';
-    border-radius: '5px';
-  `
-  export const LeftSelect = styled.div`
+export const TableContain = styled.div`
+  position: relative;
+  left: 0vw;
+  top: 10vh;
+  height: auto;
+  width: '80vw';
+  border-radius: '5px';
+`
+export const LeftSelect = styled.div`
   display: flex;
   justify-content: space-evenly;
   height: 74px;
   padding: 10px;
 `
-  export const Icon = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    text-align: center;
-    padding: 5px;
-    margin-right: 25px;
-    margin-left: 10px;
-  `
+export const Icon = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  text-align: center;
+  padding: 5px;
+  margin-right: 25px;
+  margin-left: 10px;
+`
