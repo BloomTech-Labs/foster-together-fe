124cafcaab81a0c733165c540e6ee0a0384cf57f
jarrodskahill <jarrodskahill@gmail.com>
2020-03-05 18:22:48 -0800

    Starting on title and search above table

--- src/components/AdminDash/Table/NeighborTable.js
+++ src/components/AdminDash/Table/NeighborTable.js
@@ -3,13 +3,18 @@ import map from './DataMap'
 import { columns } from './TableHead'
 import Table from './Table'
 import { Styles } from './TableStyle'
+import TableHeader from './TableTitle'
+import { TableCtn } from './TitleStyle'
 
 export default function EnhancedTable(props) {
   const data = map(props)
 
   return (
-    <Styles>
-      <Table columns={columns} data={data} props={props} />
-    </Styles>
+    <TableCtn>
+      <TableHeader />
+      <Styles>
+        <Table columns={columns} data={data} props={props} />
+      </Styles>
+    </TableCtn>
   )
 }
