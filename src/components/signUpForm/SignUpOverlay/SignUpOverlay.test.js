cede5c3340604a3c703df9f55bf818303867ca09
B Severino <biancasev@gmail.com>
2020-03-09 17:10:23 -0400

    Added ui component tests

--- src/components/signUpForm/SignUpOverlay/SignUpOverlay.test.js
+++ src/components/signUpForm/SignUpOverlay/SignUpOverlay.test.js
@@ -1,17 +1,16 @@
-// import React from 'react'
-// import { fireEvent } from '@testing-library/react'
-// import { renderWithReduxAndRouter as render } from '../../utils/renderWithReduxAndRouter'
-// import SignUp from './SignUpOverlay'
+import React from 'react'
+import { fireEvent } from '@testing-library/react'
+import { renderWithReduxAndRouter as render } from '../../../utils/renderWithReduxAndRouter'
+import SignUp from './SignUpOverlay'
 
-// test('renders SignUp without crashing', () => {
-//   render(<SignUp />)
-// })
+test('renders SignUp without crashing', () => {
+  render(<SignUp />)
+})
 
-// test('renders ui components', () => {
-//   const { getByText, getByPlaceholderText } = render(<SignUp />)
+test('renders ui components', () => {
+  const { getByText } = render(<SignUp />)
 
-//   getByText(/sign up/i)
-//   getByText(/next/i)
-// })
-
-test('is a fake test', () => {})
+  getByText(/next/i)
+  getByText(/log in/i)
+  getByText(/registering as a foster neighbor/i)
+})
