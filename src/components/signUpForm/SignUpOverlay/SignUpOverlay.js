986eb0439351f8f51592d7911bf7bcb770b84679
B Severino <biancasev@gmail.com>
2020-03-09 17:55:42 -0400

    Broke up sign up overlay into smaller components

--- src/components/signUpForm/SignUpOverlay/SignUpOverlay.js
+++ src/components/signUpForm/SignUpOverlay/SignUpOverlay.js
@@ -2,36 +2,16 @@ import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { axiosWithBaseURL } from '../../../Auth/axiosWithBaseUrl'
 import { PageView } from '../../../Analytics'
-//styled components
-import {
-  Container,
-  Sidebar,
-  MainContent,
-  Logo,
-  SidebarTitle,
-  Instructions,
-  ListItem,
-  BackArrow,
-  TabContainer,
-  Tab,
-  Stepper,
-  Step,
-  StepLabel,
-  StepTitle,
-  StepConnector,
-  BtnContainer,
-  BackBtn,
-  NextBtn,
-} from '../styles/signUpOverlayStyles'
-//images
-import logo from '../../../images/logo.svg'
-import backArrow from '../../../images/icons/back-arrow.svg'
-// child components
+import { Container, MainContent } from '../styles/signUpOverlayStyles'
+import Sidebar from './Sidebar'
+import Tabs from './Tabs'
+import Stepper from './Stepper'
+import Buttons from './Buttons'
 import ContactInfo from '../signUpComponents/ContactInfo'
 import LocationInfo from '../signUpComponents/LocationInfo'
 import ReviewInfo from '../signUpComponents/ReviewInfo'
 
-export default function SignUp(props) {
+export default function SignUp() {
   const history = useHistory()
   const [activeStep, setActiveStep] = useState(0)
   const [passwordConfirm, setPasswordConfirm] = useState('')
@@ -145,67 +125,18 @@ export default function SignUp(props) {
 
   return (
     <Container>
-      <Sidebar>
-        <div>
-          <Logo>
-            <img src={logo} alt='Foster Together logo' />
-          </Logo>
-          <SidebarTitle>Registering as a Foster Neighbor</SidebarTitle>
-          <Instructions>
-            <ListItem>Fill out the application below.</ListItem>
-            <ListItem>
-              We’ll email or call to confirm your spot, and set up your
-              background check.
-            </ListItem>
-            <ListItem>Process your background check online ($20).</ListItem>
-            <ListItem>
-              We’ll send an outline of our agenda a few days before the
-              training.
-            </ListItem>
-          </Instructions>
-        </div>
-        <BackArrow>
-          <img src={backArrow} alt='Back arrow icon' />
-        </BackArrow>
-      </Sidebar>
+      <Sidebar />
       <MainContent>
-        <TabContainer>
-          <Tab onClick={() => history.push('/login')}>
-            <span>Log In</span>
-          </Tab>
-          <Tab active>
-            <span>Register</span>
-          </Tab>
-        </TabContainer>
-        <Stepper>
-          {steps.map((step, index) => {
-            // returns true or false and determines the color of the step
-            const active = activeStep >= index
-            return (
-              <Step>
-                {index > 0 && <StepConnector active={active} />}
-                <StepLabel active={active}>{index + 1}</StepLabel>
-                <StepTitle active={active}>{step}</StepTitle>
-              </Step>
-            )
-          })}
-        </Stepper>
+        <Tabs />
+        <Stepper steps={steps} activeStep={activeStep} />
         <>
-          {activeStep === steps.length ? (
-            history.push('/home')
-          ) : (
-            <>
-              {GetStepContent(activeStep)}
-              <BtnContainer>
-                {activeStep !== 0 && (
-                  <BackBtn onClick={handleBack}>Back</BackBtn>
-                )}
-                <NextBtn onClick={handleNext}>
-                  {activeStep === steps.length - 1 ? 'Confirm' : 'Next'}
-                </NextBtn>
-              </BtnContainer>
-            </>
-          )}
+          {GetStepContent(activeStep)}
+          <Buttons
+            steps={steps}
+            activeStep={activeStep}
+            handleNext={handleNext}
+            handleBack={handleBack}
+          />
         </>
       </MainContent>
     </Container>
