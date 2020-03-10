8e7f63e910433c0770cb4b67bf60b7fc9095c996
Brendan <brendanschatz97@gmail.com>
2020-03-10 12:13:39 -0400

    PLAYGROUND!!!

--- src/components/AdminDash/Welcome.js
+++ src/components/AdminDash/Welcome.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BounceRight } from 'animate-components'
 import { WelcomeDiv, Title, Name } from './WelcomeStyles'
 
-export default function WelcomeMessage(props) {
+export default function WelcomeMessage() {
   return (
     <WelcomeDiv>
       <BounceRight as='div' iterations='1'>
