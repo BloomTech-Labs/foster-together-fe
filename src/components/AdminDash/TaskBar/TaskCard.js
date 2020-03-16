e20f0ccf6938ea1fdd1aae0a235c291914cce38b
B Severino <biancasev@gmail.com>
2020-03-16 13:45:36 -0400

    Converted icons on task bar

--- src/components/AdminDash/TaskBar/TaskCard.js
+++ src/components/AdminDash/TaskBar/TaskCard.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import { Card, CardTop, Number, Description } from './taskStyles'
-import { PaperMagnify, Shield, PersonCheck, HouseMagnify } from './taskIcons'
 
 const TaskCard = ({ task }) => {
   return (
     <Card>
       <CardTop>
         <Number>{task.number}</Number>
-        {task.id === 1 ? (
-          <PaperMagnify />
-        ) : task.id === 2 ? (
-          <Shield />
-        ) : task.id === 3 ? (
-          <PersonCheck />
-        ) : (
-          <HouseMagnify />
-        )}
+        <img src={task.image} alt={task.alt} />
       </CardTop>
       <Description>{task.description}</Description>
     </Card>
---
+++ src/components/AdminDash/TaskBar/icons/index.js
@@ -0,0 +1,6 @@
+import houseMagnify from './house-magnify.svg'
+import paperMagnify from './paper-magnify.svg'
+import personCheck from './person-check.svg'
+import shield from './shield.svg'
+
+export { houseMagnify, paperMagnify, personCheck, shield }
