00e52fe03983530b8e2138d0b917552baba868f4
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-17 01:07:22 -0700

    fixed minor linter error

--- src/components/AdminDash/Table/DataMap.js
+++ src/components/AdminDash/Table/DataMap.js
@@ -4,22 +4,22 @@ import families from '../AdminDashPics/fosterFamily.png'
 export default function mapping(props) {
   const rows = []
   function TableRowData(DataSet, Type, Picture, id) {
-    DataSet.map(data => {
-        rows.push({
-          name: `${data.first_name}, ${data.last_name}`,
-          type: Picture,
-          match: 'none',
-          city: data.city,
-          userType: Type,
-          application: true,
-          background: data.background,
-          training: data.training,
-          email: data.email,
-          phone: data.phone,
-          neighbor_id: data.neighbor_id,
-          family_id: data.family_id
-        })
-    })
+    DataSet.map(data =>
+      rows.push({
+        name: `${data.first_name}, ${data.last_name}`,
+        type: Picture,
+        match: 'none',
+        city: data.city,
+        userType: Type,
+        application: true,
+        background: data.background,
+        training: data.training,
+        email: data.email,
+        phone: data.phone,
+        neighbor_id: data.neighbor_id,
+        family_id: data.family_id,
+      })
+    )
   }
   TableRowData(props.Neighbors, 'neighbor', neighbors)
   TableRowData(props.Families, 'family', families)
