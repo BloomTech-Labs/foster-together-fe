7f80de3ecd56f3db920d1498f0dbf09492fd9036
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-17 10:14:54 -0700

    fixed failing memberstable test

--- src/components/AdminDash/Table/MemberTable.test.js
+++ src/components/AdminDash/Table/MemberTable.test.js
@@ -2,38 +2,8 @@ import React from 'react'
 import { renderWithReduxAndRouter as render } from '../../../utils/testHelpers'
 import MemberTable from './MemberTable'
 
-const neighbors = []
-const families = []
+const membersArray = []
 
 test('renders MemberTable without crashing', () => {
-  render(<MemberTable userType='neighbors' Data={neighbors} Data2={families} />)
-})
-
-const data = {
-  Data: [{ first_name: 'bob', last_name: 'smith' }, {}],
-  Data2: [{}, {}],
-}
-
-test('length is four', () => {
-  const expected = 4
-  const actual = DataMap(data).length
-  expect(expected).toEqual(actual)
-})
-
-test('test that name is coming through correctly', () => {
-  const expected = 'smith, bob'
-  const actual = DataMap(data)[0].name
-  expect(expected).toEqual(actual)
-})
-
-test('test that match is coming through as none', () => {
-  const expected = 'none'
-  const actual = DataMap(data)[0].match
-  expect(expected).toEqual(actual)
-})
-
-test('renders MemberTable without crashing', () => {
-  const neighbors = []
-  const props = {}
-  render(<MemberTable columns={columns} data={neighbors} props={props} />)
+  render(<MemberTable members={membersArray} />)
 })
