7464b93fe6cc8143bf98c8b1cd71b13ac6d63a4b
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-17 01:07:43 -0700

    added untracked files

---
+++ src/redux/slices/memSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const memSlice = createSlice({
+  name: 'mem',
+  initialState: {
+    membersArray: [],
+    memError: null,
+    selectedMember: {},
+  },
+  reducers: {
+    setMembersArray(state, action) {
+      state.membersArray = action.payload
+    },
+    setMemError(state, action) {
+      state.memError = action.payload
+    },
+    setSelectedMember(state, action) {
+      state.selectedMember = action.payload
+    },
+  },
+})
+
+export const {
+  setMembersArray,
+  setMemError,
+  setSelectedMember,
+} = memSlice.actions
+export default memSlice.reducer
