52cabad2644174bd3af3d2da3be6f46f63b65d28
B Severino <biancasev@gmail.com>
2020-03-17 18:20:34 -0400

    Cleaned up sign up components

--- src/components/signUpForm/signUpComponents/ReviewInfo.js
+++ src/components/signUpForm/signUpComponents/ReviewInfo.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Flex } from '../../../GlobalStyles'
 import {
   ReviewContainer,
   BtnContainer,
   Reviewtextone,
   CityStateZip,
 } from '../styles/reviewStyles'
-import { RadioButton } from './RadioButton'
+import { LabelContainer, RadioSpan } from '../styles/radioButtonStyles'
+import { handleCheck } from './handleCheck'
+
+const RadioButton = ({ setIsNeighbor }) => {
+  const [checked, setChecked] = useState({ family: true, neighbor: false })
+  useEffect(() => {
+    setIsNeighbor(checked.neighbor)
+  }, [setIsNeighbor, checked])
+
+  return (
+    <Flex>
+      <LabelContainer
+        onClick={() => handleCheck(checked, setChecked, 'neighbor')}
+        class='container'
+      >
+        Neighbor
+        <RadioSpan checked={checked.neighbor}></RadioSpan>
+      </LabelContainer>
+      <LabelContainer
+        onClick={() => handleCheck(checked, setChecked, 'family')}
+        class='container'
+      >
+        Family
+        <RadioSpan checked={checked.family}></RadioSpan>
+      </LabelContainer>
+    </Flex>
+  )
+}
 
 const ReviewInfo = ({ setIsNeighbor, values }) => (
   <div>
