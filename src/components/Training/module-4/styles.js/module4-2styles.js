f58e8378bb7b4f318bcf7843e373da246cc3dc7f
stevedole808 <stevedolelambda@gmail.com>
2020-03-23 06:20:10 -1000

    added module 4.2 styles

---
+++ src/components/Training/module-4/styles.js/module4-2styles.js
@@ -0,0 +1,162 @@
+import styled from 'styled-components'
+
+export const Page = styled.div`
+  font-size: 1.8rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 64%;
+  margin: auto;
+`
+
+export const Header = styled.h1`
+  display: flex;
+  flex-direction: row;
+  font-size: 2.5rem;
+  width: 50%;
+  font-weight: 400;
+  margin: 6rem 0rem 4rem 0rem;
+  padding-left: 15%;
+  svg {
+    margin: 0.5rem 1.5rem 0rem 0rem;
+  }
+`
+
+export const Body = styled.div`
+  flex-direction: coloumn;
+  font-size: 1.8rem;
+  line-height: 2.9rem;
+  margin-bottom: 1rem;
+`
+export const Video = styled.div`
+  margin-left: 10rem;
+`
+
+export const Body1 = styled.text`
+  font-size: 1.8rem;
+  line-height: 3rem;
+  width: 100%;
+  margin-bottom: 1rem;
+`
+
+export const MainContent = styled.div`
+  display: flex;
+  width: 100%;
+  flex-dirction: row;
+`
+
+export const MainContent1 = styled.text`
+  line-height: 2.5rem;
+  width: 42%;
+  margin: 0rem 5rem 0rem 3rem;
+`
+
+export const Body2 = styled.text`
+  font-size: 1.8rem;
+  line-height: 3rem;
+`
+export const Half = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 1rem;
+`
+
+export const Question = styled.text`
+  font-size: 2rem;
+  text-align: left;
+  margin-bottom: 3rem;
+`
+export const MainContent2 = styled.text`
+  font-size: 1.8rem;
+  line-height: 2.5rem;
+`
+
+export const Section3 = styled.text`
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+`
+
+export const Header3 = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 400;
+  margin: 5rem 0rem 4rem 0rem;
+`
+
+export const Image2 = styled.img``
+
+export const Span = styled.text`
+  font-weight: 500;
+  font-size: 1.8rem;
+`
+
+export const Section2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 1.8rem;
+`
+export const Input = styled.textarea`
+  width: 100%;
+  height: 125px;
+  placeholderTextColor: "red"
+}),
+  background-color: "#000";
+`
+export const Submit = styled.button`
+  font-size: 1.8rem;
+  color: white;
+  background-color: #385763;
+  width: 20rem;
+  height: 5rem;
+  &:hover {
+    color: black;
+    cursor: pointer;
+  }
+  margin: 5rem 0rem 10rem 0rem;
+`
+export const ClickHere = styled.text`
+  color: grey;
+  font-size: 1.4rem;
+`
+
+export const section3 = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  width: 100%;
+`
+
+export const ATag = styled.a`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  text-decoration: none;
+`
+export const Header2 = styled.h1`
+  text-align: center;
+  margin-top: 3rem;
+  display: flex;
+  justify-content: flex-start;
+  font-size: 1.5rem;
+  font-weight: 300px;
+`
+
+export const Body3 = styled.h1`
+  font-size: 1.5rem;
+`
+
+export const Link = styled.a`
+  color: black;
+  text-decoration: none;
+  color: Grey;
+`
+
+export const Quote = styled.p`
+  font-size: 1.6rem;
+  text-align: left;
+  font-style: italic;
+  margin-left: 2.5rem;
+`
+
+export const Statement = styled.h1`
+  font-size: 1.5rem;
+`
