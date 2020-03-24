a8b7b2f43bf9615846f1a39f825307781c442db9
jarrodskahill <jarrodskahill@gmail.com>
2020-03-24 10:41:02 -0700

    removed extra div

--- src/components/AdminDash/AdminSearch/search.js
+++ src/components/AdminDash/AdminSearch/search.js
@@ -3,13 +3,11 @@ import { SearchDiv } from './searchStyles'
 
 const SearchBar = () => {
   return (
-    <div>
-      <SearchDiv>
-        <h1>People</h1>
-        <a>Latest Activity</a>
-        <a>Alphabetical</a>
-      </SearchDiv>
-    </div>
+    <SearchDiv>
+      <h1>People</h1>
+      <a>Latest Activity</a>
+      <a>Alphabetical</a>
+    </SearchDiv>
   )
 }
 
