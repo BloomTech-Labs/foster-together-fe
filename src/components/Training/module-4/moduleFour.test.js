ca082180f0c61a0b1ae92cb9b36d95f2014adc6a
jarrodskahill <jarrodskahill@gmail.com>
2020-03-26 11:32:33 -0700

    test coverage up to 79%

--- src/components/Training/module-4/moduleFour.test.js
+++ src/components/Training/module-4/moduleFour.test.js
@@ -4,6 +4,17 @@ import { Module41 } from './components/module4-1'
 import { Module42 } from './components/module4-2'
 import { Module43 } from './components/module4-3'
 import { Module44 } from './components/module4-4'
+import ModuleFour from '.'
+
+const handleNext = (e, { activeStep, setActiveStep }) => {
+  e.preventDefault()
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+  if (activeStep === 0 || activeStep === 1 || activeStep === 2) {
+    setActiveStep(activeStep + 1)
+  } else if (activeStep === 3) {
+    setActiveStep(0)
+  }
+}
 
 test('Module four one', () => {
   render(<Module41 />)
@@ -19,4 +30,8 @@ test('Module four three', () => {
 
 test('Module four four', () => {
   render(<Module44 />)
+})
+
+test('Module four displayed', () => {
+  render(<ModuleFour />)
 })
