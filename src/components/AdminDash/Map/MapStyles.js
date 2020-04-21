0f35f0e9617f9d48ceef879d71ec8415dfcd39fd
lihuang-zheng <jerryzheng0131@gmail.com>
2020-04-21 16:57:42 -0500

    aggressively changed the filter, map and userinfo layout.

--- src/components/AdminDash/Map/MapStyles.js
+++ src/components/AdminDash/Map/MapStyles.js
@@ -1,94 +1,65 @@
 import styled from "styled-components";
 
-/*
-=== FilterDiv (old) ===
-position: relative;
-  left: 43px;
-  top: 34px;
-*/
-
-export const FilterDiv = styled.div`
-  width: 240px;
-  height: 960px;
-  margin-left: 43px;
-  background-color: F9F9F9;
-  font-family: "Roboto";
-`;
-
-export const MapContain = styled.div`
-  height: 100vh;
-  width: 90vw;
-  background-color: black;
-`;
-
+/* =============================================================================================================== */
+/* ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ Below is for Map Page Component ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* =============================================================================================================== */
 export const PageContain = styled.div`
   display: flex;
 `;
+/* =============================================================================================================== */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ Above is for Map Page Component ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+/* =============================================================================================================== */
 
-/* === Below is for Personinfo component === */
-export const InfoDiv = styled.div`
-  width: 20vw;
-  height: 100vh;
-  background-color: F9F9F9;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding-left: 2%;
-  padding-top: 1%;
+/* ========================================================================================================== */
+/* ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ Below is for Map component ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ========================================================================================================== */
+export const MapContain = styled.div`
+  height: 92.5vh;
+  width: 70vw;
+  background-color: black;
 `;
-export const UserName = styled.div`
-  font-size: 5rem;
-  padding-top: 2%;
-  text-align: center;
-`;
+/* ========================================================================================================== */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ Above is for Map component ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+/* ========================================================================================================== */
 
-export const IconContain = styled.div`
-  display: flex;
-  align-items: flex-start;
-  justify-content: flex-start;
-  font-size: 1.2rem;
-  margin: 0px 0px 20px 6px;
-  width: 100%;
+/* ================================================================================================================ */
+/* ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ Below is for MapFilter component ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ================================================================================================================ */
+export const FilterDiv = styled.div`
+  width: 15vw;
+  height: 92.5vh;
+  padding: 3vh 2vw;
+  background-color: #f9f9f9;
+  color: #375762;
 `;
-
-export const InfoText = styled.p`
-  margin: 0px 0px 20px 6px;
-`;
-
-export const Icon = styled.img`
-  width: 16px;
-  height: 16px;
-`;
-/* === Above is for the PersonInfo component ===*/
 
 export const Buttons = styled.button`
   width: 16px;
   height: 16px;
-  margin-top: 1%;
-  background: white;
+  background: ${(props) =>
+    props.selected === props.current ? "#375762" : "white"};
   border: none;
   font-size: 1.4rem;
-  color: ${props => (props.selected === props.current ? "black" : "white")};
-  border: 2px solid black;
+  border: 1.5px solid #bdbdbd;
   border-radius: 50%;
 `;
 
 export const ButtonHolder = styled.div`
   display: flex;
   flex-direction: column;
-  //   align-items: center;
   margin-top: 1vh;
 `;
 
-export const Header = styled.h1`
+export const Header = styled.h2`
   font-size: 1.6rem;
   margin: 0;
-  padding-left: 6px;
+  color: #757575;
 `;
 
 export const Image = styled.img`
   width: 16px;
   height: 16px;
+  margin-right: 6px;
 `;
 
 export const HeaderHolder = styled.div`
@@ -105,11 +76,11 @@ export const Input = styled.input`
 export const ButtonContain = styled.div`
   display: flex;
   align-items: center;
-  width: 40%;
+  width: 100%;
 `;
 
-export const Label = styled.h1`
-  font-size: 2.2rem;
+export const Label = styled.p`
+  font-size: 1.6rem;
   margin-left: 10%;
 `;
 
@@ -118,3 +89,62 @@ export const SmallIcon = styled.img`
   position: relative;
   top: -20px;
 `;
+/* ================================================================================================================ */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ Above is for MapFilter component ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+/* ================================================================================================================ */
+
+/* ================================================================================================================= */
+/* ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ Below is for Personinfo component ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* ================================================================================================================= */
+export const InfoContainer = styled.div`
+  background-color: #f9f9f9;
+  width: 15vw;
+  height: 92.5vh;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const InfoDiv = styled.div`
+  padding: 7.5vh 2vw;
+`;
+
+export const UserName = styled.div`
+  display: flex;
+  font-size: 2rem;
+  font-weight: bold;
+  padding-top: 2%;
+  color: #676767;
+`;
+
+export const InfoType = styled.p`
+  font-size: 1.2rem;
+  font-weigth: normal;
+  color: #676767;
+  padding: 0;
+  margin: 0;
+`;
+
+export const IconContain = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  font-size: 1.2rem;
+  margin: 0px 0px 20px 0px;
+`;
+
+export const SubInfoContainer = styled.div`
+  margin: 9vh 0;
+`;
+
+export const InfoText = styled.p`
+  margin: 0px 0px 20px 9px;
+  color: #676767;
+`;
+
+export const Icon = styled.img`
+  width: 16px;
+  height: 16px;
+`;
+/* ===================================================================================================================== */
+/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ Above is for the PersonInfo component ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+/* ===================================================================================================================== */
