91485ef28c39835606e7ccf89f2a3cbb6d3e99b6
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-04-22 11:31:45 -0600

    Styled family and neighbor markers

--- src/components/AdminDash/Map/FamilyMarker.js
+++ src/components/AdminDash/Map/FamilyMarker.js
@@ -1,52 +1,52 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function FamMark(props) {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   function clicked() {
-    setOpen(true)
+    setOpen(true);
     if (open === false) {
-      props.setSelected(props.location)
+      props.setSelected(props.location);
     }
   }
 
   useEffect(() => {
     if (props.location.id === props.selected.id) {
-      setOpen(true)
+      setOpen(true);
     } else {
-      setOpen(false)
+      setOpen(false);
     }
-  }, [props.selected])
+  }, [props.selected]);
 
   return (
     <>
       {!open ? (
         <div
           onClick={() => {
-            clicked()
+            clicked();
           }}
           style={{
             width: `${props.zoom * 1.5}px`,
             height: `${props.zoom * 1.5}px`,
-            borderRadius: '50%',
-            backgroundColor: '#375762',
+            borderRadius: "50%",
+            backgroundColor: "#375762",
+            boxShadow: "0px 4px 5px grey"
           }}
         />
       ) : (
         <div
           onClick={() => {
-            clicked()
+            clicked();
           }}
           style={{
-            position: 'relative',
-            top: '-15px',
-            left: '-12px',
-            width: `${props.zoom * 4}px`,
-            height: `${props.zoom * 4}px`,
-            borderRadius: '50%',
-            backgroundColor: '#375762',
+            backgroundColor: "#375762",
+            borderRadius: "50%",
+            width: `${props.zoom * 1.5}px`,
+            height: `${props.zoom * 1.5}px`,
+            transform: "scale(2, 2)",
+            boxShadow: "0px 4px 5px grey"
           }}
         ></div>
       )}
     </>
-  )
+  );
 }
--- src/components/AdminDash/Map/NeighborMarker.js
+++ src/components/AdminDash/Map/NeighborMarker.js
@@ -1,52 +1,61 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function FamMark(props) {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   function clicked() {
-    setOpen(true)
+    setOpen(true);
     if (open === false) {
-      props.setSelected(props.location)
+      props.setSelected(props.location);
     }
   }
 
   useEffect(() => {
     if (props.location.id === props.selected.id) {
-      setOpen(true)
+      setOpen(true);
     } else {
-      setOpen(false)
+      setOpen(false);
     }
-  }, [props.selected])
+  }, [props.selected]);
 
   return (
     <>
       {!open ? (
         <div
           onClick={() => {
-            clicked()
+            clicked();
           }}
           style={{
             width: `${props.zoom * 1.5}px`,
             height: `${props.zoom * 1.5}px`,
-            borderRadius: '50%',
-            backgroundColor: '#FF8D86',
+            borderRadius: "50%",
+            backgroundColor: "#FF8D86",
+            boxShadow: "0px 4px 5px grey"
           }}
         />
       ) : (
         <div
           onClick={() => {
-            clicked()
+            clicked();
           }}
           style={{
+            backgroundColor: "#FF8D86",
+            borderRadius: "50%",
+            width: `${props.zoom * 1.5}px`,
+            height: `${props.zoom * 1.5}px`,
+            transform: "scale(2, 2)",
+            boxShadow: "0px 4px 5px grey"
+          }}
+        ></div>
+      )}
+    </>
+  );
+}
+
+/* 
+  The original style for the dot
             position: 'relative',
             top: '-15px',
             left: '-12px',
             width: `${props.zoom * 4}px`,
             height: `${props.zoom * 4}px`,
-            borderRadius: '50%',
-            backgroundColor: '#FF8D86',
-          }}
-        ></div>
-      )}
-    </>
-  )
-}
+*/
