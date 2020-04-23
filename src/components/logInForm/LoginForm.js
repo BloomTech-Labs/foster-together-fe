aa41ccb2eb96a9291be605d12420167cc8189e07
lihuang-zheng <jerryzheng0131@gmail.com>
2020-04-23 12:38:32 -0500

    Changed the timer on the login form.

--- src/components/logInForm/LoginForm.js
+++ src/components/logInForm/LoginForm.js
@@ -1,22 +1,24 @@
-import React, { useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
-import { Container, TabContainer, Tab } from '../style'
-import { ContentBox } from './styles'
-import { useSelector } from 'react-redux'
-import LoginInputs from './InputContainer'
-import LoginSideBar from './LoginSideBar'
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { Container, TabContainer, Tab } from "../style";
+import { ContentBox } from "./styles";
+import { useSelector } from "react-redux";
+import LoginInputs from "./InputContainer";
+import LoginSideBar from "./LoginSideBar";
 
 const LoginForm = () => {
-  const { push } = useHistory()
-  const { userType } = useSelector(state => state.auth)
+  const { push } = useHistory();
+  const { userType } = useSelector((state) => state.auth);
 
   useEffect(() => {
-    if (userType === 'admins') {
-      push('/dashboard')
-    } else if (userType === 'families' || userType === 'neighbors') {
-      push('/userProfile')
-    }
-  }, [userType, push])
+    setTimeout(() => {
+      if (userType === "admins") {
+        push("/dashboard");
+      } else if (userType === "families" || userType === "neighbors") {
+        push("/userProfile");
+      }
+    }, 3000);
+  }, [userType, push]);
 
   return (
     <Container>
@@ -26,14 +28,14 @@ const LoginForm = () => {
           <Tab active>
             <span>Log In</span>
           </Tab>
-          <Tab onClick={() => push('/signup')}>
+          <Tab onClick={() => push("/signup")}>
             <span>Register</span>
           </Tab>
         </TabContainer>
         <LoginInputs />
       </ContentBox>
     </Container>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
