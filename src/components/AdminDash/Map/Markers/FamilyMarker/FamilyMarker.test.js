223741de2e4640a78ea1d7344b0067e42b43317d
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-05-05 15:10:42 -0600

    Fixed minor bugs on markers + added more testing to family and neighbor marker

--- src/components/AdminDash/Map/Markers/FamilyMarker/FamilyMarker.test.js
+++ src/components/AdminDash/Map/Markers/FamilyMarker/FamilyMarker.test.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom";
 import FamilyMarker from "./FamilyMarker";
 import "@testing-library/jest-dom/extend-expect";
 
-it("renders without crashing", () => {
+const clicked = jest.fn(() => true);
+
+test("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<FamilyMarker></FamilyMarker>, div);
+});
+
+test("on click it expands", () => {
+  expect(clicked()).toBe(true);
+  expect(clicked).toHaveBeenCalledTimes(1);
 });
