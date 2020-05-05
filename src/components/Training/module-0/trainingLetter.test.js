6482783d76b9534855204dc4f4c4040810536096
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-05-05 13:50:30 -0600

    Added testing for containers, all tests passing

--- src/components/Training/module-0/trainingLetter.test.js
+++ src/components/Training/module-0/trainingLetter.test.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { renderWithReduxAndRouter as render } from '../../../utils/testHelpers'
-import ModuleStart from './trainingLetter'
-import ModuleStart2 from './trainingLetter2'
-import '@testing-library/jest-dom/extend-expect'
+import React from "react";
+import { renderWithReduxAndRouter as render } from "../../../utils/testHelpers/index";
+import ModuleStart from "./trainingLetter";
+import ModuleStart2 from "./trainingLetter2";
+import "@testing-library/jest-dom/extend-expect";
 
 import {
   MainContent,
@@ -10,22 +10,22 @@ import {
   LetterContext,
   Circle,
   Letter,
-  onClick,
-} from '../TrainStyles'
-import TrainingNav from '../TrainingNav/NavBar'
+  onClick
+} from "../TrainStyles";
+import TrainingNav from "../TrainingNav/NavBar";
 
-test('ModuleStart', async () => {
-  render(<ModuleStart />)
-})
+test("ModuleStart", async () => {
+  render(<ModuleStart />);
+});
 
-test('text is rendered', () => {
-  const { findByDisplayValue } = render(<ModuleStart />)
-  findByDisplayValue(<MainContent />, <TrainingNav />, <Letter />)
-  findByDisplayValue({ img })
-})
+test("text is rendered", () => {
+  const { findByDisplayValue } = render(<ModuleStart />);
+  findByDisplayValue(<MainContent />, <TrainingNav />, <Letter />);
+  findByDisplayValue({ img });
+});
 
-test('trainingLetter2 Letter Renders', async () => {
-  render(<ModuleStart2/>)
-})
+test("trainingLetter2 Letter Renders", async () => {
+  render(<ModuleStart2 />);
+});
 
-test('Testing onClick', async () => {})
+test("Testing onClick", async () => {});
