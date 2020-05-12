e363e738e68f24264064c541526e6e6327bdde42
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-05-12 16:01:29 -0600

    Added functions to update task bar numbers dynamically

--- src/components/AdminDash/AdminDashboard.js
+++ src/components/AdminDash/AdminDashboard.js
@@ -1,31 +1,50 @@
-import React, { useEffect } from 'react'
-import MemberTable from './Table/MemberTable'
-import Navigation from './Navigation/Navigation'
-import Welcome from './Welcome'
-import TaskBar from './TaskBar/TaskBar'
-import { DashContainer, TableContain } from './adminDashStyles'
-import { useSelector, useDispatch } from 'react-redux'
-import { getMembers } from '../../redux/thunks/memThunks'
-import SearchBar from './AdminSearch/search'
+import React, { useEffect } from "react";
+import MemberTable from "./Table/MemberTable";
+import Navigation from "./Navigation/Navigation";
+import Welcome from "./Welcome";
+import TaskBar from "./TaskBar/TaskBar";
+import { DashContainer, TableContain } from "./adminDashStyles";
+import { useSelector, useDispatch } from "react-redux";
+import { getMembers } from "../../redux/thunks/memThunks";
+import SearchBar from "./AdminSearch/search";
 
 export default function Distance() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getMembers())
-  }, [dispatch])
-  const { membersArray } = useSelector(state => state.mem)
+    dispatch(getMembers());
+  }, [dispatch]);
+  const { membersArray } = useSelector(state => state.mem);
+
+  // Functions to display taskbar //
+
+  const numApplications = membersArray.filter(
+    people => people.application === 1
+  );
+
+  const numVolunteers = membersArray.filter(
+    people => people.type === "neighbors"
+  );
+
+  const familiesToMatch = membersArray.filter(
+    people => people.type === "families"
+  );
 
   return (
     <>
       <Navigation />
       <DashContainer>
         <Welcome />
-        <TaskBar />
+        <TaskBar
+          members={membersArray}
+          numApplications={numApplications}
+          numVolunteers={numVolunteers}
+          familiesToMatch={familiesToMatch}
+        />
         <SearchBar members={membersArray} />
         <TableContain>
           <MemberTable members={membersArray} />
         </TableContain>
       </DashContainer>
     </>
-  )
+  );
 }
